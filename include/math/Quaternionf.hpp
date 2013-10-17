e04276c018915146bb4995e98e3035732883a43c
Shane del Solar <sads3@gatech.edu>
2013-10-16 23:10:23 -0400

    Renameing the quaternion files.

---
+++ include/math/Quaternionf.hpp
@@ -0,0 +1,29 @@
+#ifndef _MATH_QUATERNIONF_HPP
+#define _MATH_QUATERNIONF_HPP
+
+namespace Math
+{
+	class Quaternionf
+	{
+
+	//Variables:
+	public:
+
+		float
+
+	protected:
+	private:
+
+	//Functions:
+	public:
+
+		Quaternionf(void);
+		~Quaternionf(void);
+
+	protected:
+	private:
+
+	};
+}
+
+#endif
